aaed7a11f16a069b9043ccd877a4188f295bab4f
Lucas Cassiano de Souza <lucas.cassiano@timeguilu.com.br>
2021-08-10 11:10:35 -0300

    Adicionando as horas

--- main.py
+++ main.py
@@ -1,87 +1,43 @@
-import argparse
+from vosk import Model, KaldiRecognizer
 import os
-import queue
-import sounddevice as sd
-import vosk
-import sys
+import pyaudio
+import pyttsx3
+import json
+import core
 
-q = queue.Queue()
+# Síntese de fala
+engine = pyttsx3.init()
 
-def int_or_str(text):
-    """Helper function for argument parsing."""
-    try:
-        return int(text)
-    except ValueError:
-        return text
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
 
-def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    if status:
-        print(status, file=sys.stderr)
-    q.put(bytes(indata))
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit')
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    '-f', '--filename', type=str, metavar='FILENAME',
-    help='audio file to store recording to')
-parser.add_argument(
-    '-m', '--model', type=str, metavar='MODEL_PATH',
-    help='Path to the model')
-parser.add_argument(
-    '-d', '--device', type=int_or_str,
-    help='input device (numeric ID or substring)')
-parser.add_argument(
-    '-r', '--samplerate', type=int, help='sampling rate')
-args = parser.parse_args(remaining)
 
-try:
-    if args.model is None:
-        args.model = "model"
-    if not os.path.exists(args.model):
-        print ("Please download a model for your language from https://alphacephei.com/vosk/models")
-        print ("and unpack as 'model' in the current folder.")
-        parser.exit(0)
-    if args.samplerate is None:
-        device_info = sd.query_devices(args.device, 'input')
-        # soundfile expects an int, sounddevice provides a float:
-        args.samplerate = int(device_info['default_samplerate'])
+# Reconhecimento de fala
 
-    model = vosk.Model(args.model)
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
 
-    if args.filename:
-        dump_fn = open(args.filename, "wb")
-    else:
-        dump_fn = None
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
+stream.start_stream()
 
-    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device, dtype='int16',
-                            channels=1, callback=callback):
-            print('#' * 80)
-            print('Press Ctrl+C to stop the recording')
-            print('#' * 80)
+# Loop do reconhecimento de fala
+while True:
+    data = stream.read(2048)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        result = rec.Result()
+        result = json.loads(result)
 
-            rec = vosk.KaldiRecognizer(model, args.samplerate)
-            while True:
-                data = q.get()
-                if rec.AcceptWaveform(data):
-                    print(rec.Result())
-                else:
-                    print(rec.PartialResult())
-                if dump_fn is not None:
-                    dump_fn.write(data)
+        if result is not None:
+            text = result['text']
 
-except KeyboardInterrupt:
-    print('\nDone')
-    parser.exit(0)
-except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
+            print(text)
+
+            if text == 'que horas são' or text == 'me diga as horas':
+                speak(core.SystemInfo.get_time())
---
+++ tests/tts.py
@@ -0,0 +1,8 @@
+import pyttsx3
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+
+engine.setProperty('voice',voices[-2].id)
+engine.say("Olá, meu nome é catarina")
+engine.runAndWait()
