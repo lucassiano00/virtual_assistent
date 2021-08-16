6da741a5868c745766a0ec19b7f30312b6e25013
Lucas Cassiano de Souza <lucas.cassiano@timeguilu.com.br>
2021-08-16 10:28:28 -0300

    adicionando dia e mes

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,5 @@
 import datetime
-
+import locale
 
 
 class SystemInfo:
@@ -11,5 +11,18 @@ class SystemInfo:
     def get_time():
         now = datetime.datetime.now()
         answer = 'São {} horas e {} minutos.'.format(now.hour, now.minute)
+        
+        return answer
+
+
+    @staticmethod
+    def get_date():
+        try:
+            locale.setlocale(locale.LC_ALL, 'pt_BR')
+        except:
+            locale.setlocale(locale.LC_ALL, 'Portuguese_Brazil')
+        now = datetime.datetime.now()
+        answer = 'Hoje é dia {} de {} de {}'.format(now.day, now.strftime("%B"), now.year)
+        print(now.strftime("%B"))
         
         return answer
